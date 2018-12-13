848c0cdf73273654c484eb645a39edebf59b6154
Khanh Tran <khanhntran@users.noreply.github.com>
2018-12-13 14:25:30 -0500

    initial commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-  MONGOURI: 'mongodb://khanh:password1@ds063140.mlab.com:63140/knt_devconnector'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
